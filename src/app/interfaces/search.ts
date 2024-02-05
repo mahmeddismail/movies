8ee71a7e0f28acc6405cee51aa9fabefa34b0aed
Route <muhamedismail7777@gmail.com>
2024-02-05 20:25:39 +0200

    adding search Module&ratings&watchList

---
+++ src/app/interfaces/search.ts
@@ -0,0 +1,49 @@
+export interface Search {
+
+    adult: boolean;
+    backdrop_path?: null | string;
+    profile_path?: null | string;
+    id: number;
+    name?: string;
+    original_name?: string;
+    overview?: string;
+    poster_path?: null | string;
+    media_type: MediaType;
+    genre_ids?: number[];
+    popularity: number;
+    first_air_date?: Date;
+    vote_average?: number;
+    vote_count?: number;
+    origin_country?: string[];
+    title?: string;
+    original_title?: string;
+    release_date?: Date;
+    video?: boolean;
+    gender?: number;
+    known_for_department?: string;
+    known_for?: null | KnownFor[];
+}
+
+export enum MediaType {
+    Movie = "movie",
+    Person = "person",
+    Tv = "tv",
+}
+
+export interface KnownFor {
+    adult: boolean;
+    backdrop_path: string;
+    id: number;
+    title: string;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    poster_path: string;
+    media_type: string;
+    genre_ids: number[];
+    popularity: number;
+    release_date: Date;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
